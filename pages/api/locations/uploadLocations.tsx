75ef6cd2396e3dfd8c990678130f9cc7ba8d21b3
a.urbanite <a.urbanite@gmail.com>
2023-01-27 19:27:39 +0100

    wrote todo list

---
+++ pages/api/locations/uploadLocations.tsx
@@ -0,0 +1,9 @@
+import { getDocs } from 'firebase/firestore';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { dbRef } from '../../../firebase-config';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+}
+
+export default handler;
