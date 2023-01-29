d517b01705b9dfa205adbb487463669deff34c72
a.urbanite <a.urbanite@gmail.com>
2023-01-29 14:05:56 +0100

    FIX upload now works for polygons

--- components/FireStoreContext.js
+++ components/FireStoreContext.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { createContext } from "react";
 import { useRouter } from "next/router";
+import {
+  serializeNestedArrays,
+  deSerializeNestedArrays,
+  serializeGeoJsonCoords,
+  deserializeGeoJsonCoords,
+  convertToPrunedGeoJsonStr,
+} from "./FireStoreContext_utils";
 
 const FireStoreContext = createContext();
 
@@ -9,38 +16,6 @@ const FireStoreContextProvider = ({ children }) => {
   const [userFirestoreMarkers, setUserFirestoreMarkers] = useState(null);
   const router = useRouter();
 
-  const serializeCoordsArr = (arrayOfArrays) => {
-    // let arrayOfArrays = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-    let obj = arrayOfArrays.reduce((acc, curr, index) => {
-        acc[index] = curr;
-        return acc;
-    }, {});
-    // console.log(obj);
-    return obj
-  }
-
-  const deSerializeCoordsArr = (obj) => {
-    let array = [];
-    for (let key in obj) {
-        array.push(obj[key]);
-    }
-    return array;
-  }
-
-  const convertToPrunedGeoJsonObj = (obj) => {
-    const geoJsonObj = obj.mapLayerObj.toGeoJSON();
-    geoJsonObj.properties = {
-      id: obj.id,
-      user: obj.user,
-      dateCreated: obj.dateCreated,
-      popupContent: obj.popupContent,
-    };
-    geoJsonObj.geometry.coordinates = serializeCoordsArr(geoJsonObj.geometry.coordinates)
-    // console.log("GEOJSONOBJ ready to fly: ", geoJsonObj)
-    const geoJsonStr = JSON.stringify(geoJsonObj);
-    return geoJsonStr;
-  };
-
   //called in Markerlist/Uploadbutton
   const postDrawnmarkers = async (drawnMarkers) => {
     const response = await fetch(`http://localhost:3000/api/uploadLocations`, {
@@ -48,16 +23,18 @@ const FireStoreContextProvider = ({ children }) => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(drawnMarkers.map((obj) => convertToPrunedGeoJsonObj(obj))),
+      body: JSON.stringify(drawnMarkers.map((obj) => convertToPrunedGeoJsonStr(obj))),
     });
-    router.push('/home')
+    router.push("/home");
   };
 
   //called in home
   const fetchAllFirestoreMarkers = async () => {
     const res = await fetch(`http://localhost:3000/api/locations`);
     const markers = await res.json();
-    markers.forEach((marker) => {marker.geometry.coordinates = deSerializeCoordsArr(marker.geometry.coordinates)})
+    markers.forEach((marker) => {
+      marker.geometry.coordinates = deserializeGeoJsonCoords(marker);
+    });
     setAllFirestoreMarkers(markers);
   };
 
---
+++ components/FireStoreContext_utils.js
@@ -0,0 +1,70 @@
+const serializeNestedArrays = (arrayOfArrays) => {
+  // takes [[1,2,3],[4,5,6],[7,8,9]] returns {0:[1,2,3],1:[4,5,6],2:[7,8,9]}
+  let obj = arrayOfArrays.reduce((acc, curr, index) => {
+    acc[index] = curr;
+    return acc;
+  }, {});
+  return obj;
+};
+
+const deSerializeNestedArrays = (obj) => {
+  // takes {0:[1,2,3],1:[4,5,6],2:[7,8,9]} returns [[1,2,3],[4,5,6],[7,8,9]]
+  let array = [];
+  for (let key in obj) {
+    array.push(obj[key]);
+  }
+  return array;
+};
+
+const serializeGeoJsonCoords = (geoJsonObj) => {
+  let serializedCoordsObj;
+  switch (geoJsonObj.geometry.type) {
+    case "Point":
+      serializedCoordsObj = geoJsonObj.geometry.coordinates;
+      break;
+    case "LineString":
+      serializedCoordsObj = serializeNestedArrays(geoJsonObj.geometry.coordinates);
+      break;
+    case "Polygon":
+      serializedCoordsObj = { 0: serializeNestedArrays(geoJsonObj.geometry.coordinates[0]) };
+      break;
+  }
+  return serializedCoordsObj;
+};
+
+const deserializeGeoJsonCoords = (geoJsonObj) => {
+  let deserializedCoordsObj;
+  switch (geoJsonObj.geometry.type) {
+    case "Point":
+      deserializedCoordsObj = geoJsonObj.geometry.coordinates;
+      break;
+    case "LineString":
+      deserializedCoordsObj = deSerializeNestedArrays(geoJsonObj.geometry.coordinates);
+      break;
+    case "Polygon":
+      deserializedCoordsObj = [deSerializeNestedArrays(geoJsonObj.geometry.coordinates[0])];
+      break;
+  }
+  return deserializedCoordsObj;
+};
+
+const convertToPrunedGeoJsonStr = (obj) => {
+  const geoJsonObj = obj.mapLayerObj.toGeoJSON();
+  geoJsonObj.properties = {
+    id: obj.id,
+    user: obj.user,
+    dateCreated: obj.dateCreated,
+    popupContent: obj.popupContent,
+  };
+  geoJsonObj.geometry.coordinates = serializeGeoJsonCoords(geoJsonObj);
+  const geoJsonStr = JSON.stringify(geoJsonObj);
+  return geoJsonStr;
+};
+
+export {
+  serializeNestedArrays,
+  deSerializeNestedArrays,
+  serializeGeoJsonCoords,
+  deserializeGeoJsonCoords,
+  convertToPrunedGeoJsonStr,
+};
