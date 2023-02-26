40d9e45390f529c2a34c7a81ee7891bbb793c451
a.urbanite <a.urbanite@gmail.com>
2023-02-26 19:39:07 +0100

    fixed build errors in settings.js

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -7,8 +7,8 @@ import { auth } from '../../firebase-config';
 const Settings = () => {
   const { updateUser } = useUserContext();
 
-  const [displayname, setdisplayname] = useState<string>(auth.currentUser!.displayName!);
-  const [email, setemail] = useState<string>(auth.currentUser!.email!);
+  const [displayname, setdisplayname] = useState<string>(auth.currentUser ? auth.currentUser.displayName as string : "no_user");
+  const [email, setemail] = useState<string>(auth.currentUser ? auth.currentUser.email as string : "no_email");
   const [message, setmessage] = useState('')
   // const [photoURL, setphotoURL] = useState<string>(auth.currentUser!.photoURL!);
 
@@ -23,7 +23,7 @@ const Settings = () => {
   return (
     <div className={styles.SettingsWrapper}>
       <h1>Settings</h1>
-      <p>Welcome {auth.currentUser!.displayName}! Change your profile information here.</p>
+      <p>Welcome {displayname}! Change your profile information here.</p>
       <p>{message}</p>
       <br></br>
       {/* <img src={auth.currentUser?.photoURL?} className="profilePic" alt='profilePic'></img> */}
@@ -33,7 +33,7 @@ const Settings = () => {
           id='displayname' 
           className={styles.settingsForm__input}
           onChange={(event) => {setdisplayname(event.target.value)}} 
-          defaultValue={auth.currentUser ? auth.currentUser.displayName! : "currently no name registered"}/>
+          defaultValue={displayname}/>
         {/* <label htmlFor="photoURL">photo URL:</label>
         <input id='photoURL' onChange={(event) => {setphotoURL(event.target.value)}} defaultValue={photoURL}></input> */}
         <label htmlFor="email">Email:</label>
@@ -42,7 +42,7 @@ const Settings = () => {
           id='email' 
           className={styles.settingsForm__input}
           onChange={(event) => {setemail(event.target.value)}} 
-          defaultValue={auth.currentUser?.email!}/>
+          defaultValue={email}/>
         <input type="submit" value="Go!" className={styles.settingsForm__submit}/>
       </form>
     </div>
