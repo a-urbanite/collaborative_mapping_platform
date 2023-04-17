36b7a47cf872651b075558b2cfe372aee867ae1f
a.urbanite <a.urbanite@gmail.com>
2023-04-17 21:57:48 +0200

    implemented contribute page

--- components/MainNav/MainNav.tsx
+++ components/MainNav/MainNav.tsx
@@ -11,6 +11,7 @@ const MainNav = () => {
     <nav className={styles.mainNav}>
       <ul className={styles.list}>
         <MenuPoint href="/home">Home</MenuPoint>
+        <MenuPoint href="/contribute">contribute</MenuPoint>
         {isAuth && <MenuPoint href="/myPlaces">My Places</MenuPoint>}
         {!isAuth && <MenuPoint href="/login">Log in</MenuPoint>}
         {isAuth && <MenuPoint href="/home" func={() => signOutUser()}>Log out</MenuPoint>}
