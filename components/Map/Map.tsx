713728e4f4920905c825b7326040a168f252da6d
a.urbanite <a.urbanite@gmail.com>
2023-04-19 18:20:54 +0200

    correceted typo in tilemap attribution

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -31,7 +31,7 @@ const Map = () => {
       /> */}
 
       <TileLayer
-        attribution='&copy; <&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
 
