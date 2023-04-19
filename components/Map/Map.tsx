36a6235cf82216b0df78bc726132724fbd8813aa
a.urbanite <a.urbanite@gmail.com>
2023-04-19 18:16:38 +0200

    integrated dark theme base map

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -25,9 +25,14 @@ const Map = () => {
 
       {router.pathname === "/myPlaces" && <EditControlFC/>}
       
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /> */}
+
+      <TileLayer
+        attribution='&copy; <&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
 
 
