0ed4486343f07a8111f21c750152dfb6b52cf4cc
a.urbanite <a.urbanite@gmail.com>
2023-05-24 19:02:51 +0200

    refactored contriibute page

--- pages/contribute/index.tsx
+++ pages/contribute/index.tsx
@@ -1,31 +1,22 @@
-import React from 'react'
-import MapLoader from '../../components/Map/MapLoader'
-import MarkerList from '../../components/MarkerList/MarkerList'
-import UploadButton from '../../components/MarkerList/UploadButton/UploadButton'
-import styles from './contribute.module.scss'
-import { useModal } from '../../components/Modal/ModalContext'
+import React from "react";
+import MapLoader from "../../components/Map/MapLoader";
+import MarkerList from "../../components/MarkerList/MarkerList";
+import UploadButton from "../../components/MarkerList/UploadButton/UploadButton";
+import styles from "./contribute.module.scss";
 
 const Contribute = () => {
-  const { openModalWithError } = useModal();
-
-  const handleButtonClick = async () => {
-    const submittedName = await openModalWithError("asdsad");
-    console.log("Submitted name:", submittedName);
-  };
-
   return (
     <>
-      <button onClick={handleButtonClick}>Open Modal</button>
       <p>Contribute</p>
       <div className={styles.homeContainer}>
-      <MapLoader markers={[]}/>
-      <div className={styles.sidebar}>
-        <MarkerList />
-        <UploadButton/>
+        <MapLoader markers={[]} />
+        <div className={styles.sidebar}>
+          <MarkerList />
+          <UploadButton />
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Contribute
+export default Contribute;
