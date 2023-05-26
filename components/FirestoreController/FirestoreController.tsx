bde91ee707ddbcc9bbdb35ff19656cf87663e02a
a.urbanite <a.urbanite@gmail.com>
2023-05-26 18:19:34 +0200

    refactored uploadEdits() to clean code standards

--- components/FirestoreController/FirestoreController.tsx
+++ components/FirestoreController/FirestoreController.tsx
@@ -14,71 +14,60 @@ interface ProviderProps {
   children: ReactNode;
 }
 
-
 const FirestoreController = React.createContext(null as unknown as ContextProps);
 
 const FirestoreControllerProvider = ({ children }: ProviderProps) => {
   const [markersUpdated, setmarkersUpdated] = React.useState<boolean>(false);
   const [initialFetch, setinitialFetch] = React.useState<boolean>(true);
 
-  //forms map into array
-  //filters out non-changed markers
-  //reshapes the geojson to fit firestorestandards
-
-  const prepareMarkersToUpload = (markerMap: MarkerMap) => {
-    const markersToUploadArr: FirestoreMarker[] = [];
-    markerMap.forEach((marker) => {
-      if (marker.properties.operationIndicator !== null) {
-        delete marker.mapLayerObj;
-        marker.geometry.coordinates = serializeGeoJsonCoords(marker);
-        markersToUploadArr.push(marker);
-      }
-    });
-    return markersToUploadArr;
-  };
-
   const uploadEdits = async (userFirestoreMarkers: MarkerMap) => {
     try {
-      const markersToUpload = prepareMarkersToUpload(userFirestoreMarkers);
-  
+      const reshapedGeoJsonArr = Array.from(userFirestoreMarkers.values())
+        .filter((marker) => marker.properties.operationIndicator !== null)
+        .map((marker) => ({
+          ...marker,
+          mapLayerObj: undefined,
+          geometry: {
+            type: marker.geometry.type,
+            coordinates: serializeGeoJsonCoords(marker),
+          },
+        }));
+
       const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/uploadEdits`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(markersToUpload),
+        body: JSON.stringify(reshapedGeoJsonArr),
       });
       if (!res.ok) throw new Error(`HTTP error: ${res.status}`);
       const body = await res.json();
-  
+
       setmarkersUpdated(true);
       return body;
-
     } catch (err) {
-        throw new Error("Upload Error", { cause: err }); 
+      throw new Error("Upload Error", { cause: err });
     }
   };
 
   const fetchAllMarkers = async () => {
-
     try {
       setmarkersUpdated(false);
       setinitialFetch(false);
-  
+
       const res = await fetch(`${process.env.NEXT_PUBLIC_HOST_URL}/api/locations`);
       if (!res.ok) throw new Error(`HTTP error: ${res.status}, ${res.statusText}`);
       const body = await res.json();
-  
+
       const markerMap = new Map();
       body.forEach((marker: FirestoreMarker) => {
-        marker.geometry.coordinates = deserializeGeoJsonCoords(marker)
+        marker.geometry.coordinates = deserializeGeoJsonCoords(marker);
         markerMap.set(marker.properties.markerId, marker);
       });
-  
-      return markerMap;
 
+      return markerMap;
     } catch (err) {
-        throw new Error("Fetch Error", { cause: err });
+      throw new Error("Fetch Error", { cause: err });
     }
   };
 
