4079663fa644daf467e813db2ff1989fcd6b6ef6
a.urbanite <a.urbanite@gmail.com>
2023-06-28 17:01:32 +0200

    implemented title search on orderNum only url

--- pages/story/[orderNum]/index.tsx
+++ pages/story/[orderNum]/index.tsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { useRouter } from "next/router";
 import { useMarkerContext } from '../../../components/Map/MarkerContext';
+import { useModal } from '../../../components/Modal/ModalContext';
+import { useFirestoreController } from '../../../components/FirestoreController/FirestoreController';
 
 const Index = () => {
   const router = useRouter();
   const { findMarkerByOrderNum, allMarkers } = useMarkerContext();
+  const { openModalWithSpinner, closeModal, openModalWithError } = useModal();
+  const { fetchSingleMarker } = useFirestoreController();
 
   React.useEffect(() => {
+    if (!router.isReady) return;
+    openModalWithSpinner("Fetching Marker...");
     const orderNum = Number(router.query.orderNum);
-    const marker = findMarkerByOrderNum(allMarkers, orderNum);
-    if (marker) {
-      const urlTitle = marker.properties.popupContent.title.replace(" ","_")
-      router.push(`/${orderNum}/${urlTitle}`)
-      console.log("inside IF ")
-    } else {
-      router.push("/nooooo")
-    }
-    // router.push("test")
+    fetchSingleMarker(orderNum, "")
+      .then((marker) => {
+        closeModal();
+        const title = marker.properties.popupContent.title.replace(" ","_")
+        router.push(`/story/${orderNum}/${title}`)
+      })
+      .catch((e) => {
+        openModalWithError(`Server Error: (${e.cause})`);
+      });
   }, [router.isReady]);
 
   return (
